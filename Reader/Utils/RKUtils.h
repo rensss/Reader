aa7900a0f8b424d7b683bc106c5f0eb761538121
Rzk <renzzz@qq.com>
2020-06-04 17:29:06 +0800

    添加实用类   方法

---
+++ Reader/Utils/RKUtils.h
@@ -0,0 +1,17 @@
+//
+//  RKUtils.h
+//  Reader
+//
+//  Created by Rzk on 2020/6/4.
+//  Copyright © 2020 Rzk. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface RKUtils : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ Reader/Utils/RKUtils.m
@@ -0,0 +1,13 @@
+//
+//  RKUtils.m
+//  Reader
+//
+//  Created by Rzk on 2020/6/4.
+//  Copyright © 2020 Rzk. All rights reserved.
+//
+
+#import "RKUtils.h"
+
+@implementation RKUtils
+
+@end
