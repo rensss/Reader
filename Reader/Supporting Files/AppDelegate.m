c5576cae74a31f4e433d067f3dd5ce9503ee2fcb
Rzk <renzzz@qq.com>
2020-08-08 17:19:58 +0800

    fix 打印

--- Reader/Supporting Files/AppDelegate.m
+++ Reader/Supporting Files/AppDelegate.m
@@ -39,8 +39,6 @@
         [DDLog addLogger:[DDTTYLogger sharedInstance]];
         [DDTTYLogger sharedInstance].logFormatter = [[RKLogFormatter alloc] init];
     }
-    // 添加DDTTYLogger，你的日志语句将被发送到Console.app
-    [DDLog addLogger:[DDASLLogger sharedInstance]];
     // 添加DDFileLogger，你的日志语句将写入到一个文件中，默认路径在沙盒的Library/Caches/Logs/目录下，文件名为bundleid+空格+日期.log。
     DDFileLogger *fileLogger = [[DDFileLogger alloc] init];
     fileLogger.rollingFrequency = 60 * 60 * 24;
--- Reader/Views/RKReadStatusBar.m
+++ Reader/Views/RKReadStatusBar.m
@@ -112,7 +112,7 @@
     
     float level = device.batteryLevel;
     
-    DDLogInfo(@"---- level = %f",level);
+//    DDLogInfo(@"---- level = %f",level);
     
     self.batteryNum.text = [NSString stringWithFormat:@"%.0f",level*100];
     if (level > 0 && level < 0.2f) {
