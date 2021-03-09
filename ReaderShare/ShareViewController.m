21c7eb02e5088885db87a04e2f797e52867aeaa1
Rzk <renzzz@qq.com>
2021-03-09 14:09:01 +0800

    feat 分享

--- ReaderShare/ShareViewController.m
+++ ReaderShare/ShareViewController.m
@@ -8,12 +8,70 @@
 
 #import "ShareViewController.h"
 
+#define kAPPGroupName @"group.micsoft.Reader"
+#define kScreenWidth [UIScreen mainScreen].bounds.size.width
+#define kScreenHeight [UIScreen mainScreen].bounds.size.height
+
 @interface ShareViewController ()
+
+
 
 @end
 
 @implementation ShareViewController
 
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    self.view.backgroundColor = [UIColor whiteColor];
+    
+    UIButton *addButton = [[UIButton alloc] initWithFrame:CGRectMake((kScreenWidth-100)/2, 100, 100, 45)];
+    addButton.backgroundColor = [UIColor redColor];
+    [addButton setTitle:@"添加" forState:UIControlStateNormal];
+    [addButton addTarget:self action:@selector(btnClick:) forControlEvents:UIControlEventTouchUpInside];
+    [self.view addSubview:addButton];
+}
+
+
+#pragma mark - event
+- (void)btnClick:(UIButton *)btn {
+    [self.extensionContext.inputItems enumerateObjectsUsingBlock:^(id  _Nonnull obj, NSUInteger idx, BOOL * _Nonnull stop) {
+        /*
+         attributedTitle:(null)
+         attributedContentText:(null)
+         attachments:(
+             "<NSItemProvider: 0x2839e8e00> {
+                types = (\n    \"public.plain-text\",
+                        \n    \"public.file-url\"\n)
+                }"
+         */
+        if ([obj isKindOfClass:[NSExtensionItem class]]) {
+            NSExtensionItem *item = obj;
+            NSLog(@"\nidx:%lu\nattributedTitle:%@\nattributedContentText:%@\nattachments:%@", (unsigned long)idx, item.attributedTitle, item.attributedContentText, item.attachments);
+            
+            [item.attachments enumerateObjectsUsingBlock:^(NSItemProvider * _Nonnull obj, NSUInteger idx, BOOL * _Nonnull stop) {
+                if ([obj hasItemConformingToTypeIdentifier:@"public.plain-text"]) {
+                    [obj loadItemForTypeIdentifier:@"public.plain-text" options:nil completionHandler:^(__kindof id<NSSecureCoding>  _Nullable item, NSError * _Null_unspecified error) {
+                        NSLog(@"item:%@\n", [item class]);
+                        
+                         NSURL *groupURL = [NSFileManager.defaultManager containerURLForSecurityApplicationGroupIdentifier:kAPPGroupName];
+                        [NSFileManager.defaultManager moveItemAtURL:item toURL:groupURL error:nil];
+                    }];
+                }
+                if ([obj hasItemConformingToTypeIdentifier:@"public.file-url"]) {
+                    [obj loadItemForTypeIdentifier:@"public.file-url" options:nil completionHandler:^(__kindof id<NSSecureCoding>  _Nullable item, NSError * _Null_unspecified error) {
+                        NSLog(@"item:%@\n", [item class]);
+                    }];
+                }
+            }];
+            
+        }
+//        *stop = YES;
+    }];
+
+}
+
+
+#pragma mark - system func
 - (BOOL)isContentValid {
     // Do validation of contentText and/or NSExtensionContext attachments here
     return YES;
