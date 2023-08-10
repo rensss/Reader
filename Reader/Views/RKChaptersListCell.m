62a39836286efa01dc2a4d5efb0703276b790a98
Rzk <renzzz@qq.com>
2023-08-10 12:18:17 +0800

    fix 目录滚动 bug

--- Reader/Views/RKChaptersListCell.m
+++ Reader/Views/RKChaptersListCell.m
@@ -12,6 +12,7 @@
 
 @property (nonatomic, strong) RKMarqueeView *titleMarqueeView; /**< 标题*/
 @property (nonatomic, strong) UILabel *titleView; /**< 章节名*/
+@property (nonatomic, assign) CGFloat itemViewWidth; /**< title 宽度缓存 */
 
 @end
 
@@ -22,18 +23,19 @@
     if (self) {
         [self.contentView addSubview:self.titleMarqueeView];
         [self.titleMarqueeView mas_makeConstraints:^(MASConstraintMaker *make) {
-            make.top.equalTo(self.contentView.mas_top).mas_offset(0);
-            make.bottom.equalTo(self.contentView.mas_bottom).mas_offset(0);
-            make.leading.equalTo(self.contentView.mas_leading).mas_offset(8);
-            make.trailing.equalTo(self.contentView.mas_trailing).mas_offset(-8);
+            make.top.mas_offset(0);
+            make.left.mas_offset(8);
+            make.bottom.mas_offset(0);
+            make.right.mas_offset(-8);
         }];
-//        [self.contentView addSubview:self.titleView];
-//        [self.titleView mas_makeConstraints:^(MASConstraintMaker *make) {
-//            make.top.mas_offset(0);
-//            make.left.mas_offset(8);
-//            make.bottom.mas_offset(0);
-//            make.right.mas_offset(-8);
-//        }];
+        
+        [self.contentView addSubview:self.titleView];
+        [self.titleView mas_makeConstraints:^(MASConstraintMaker *make) {
+            make.top.mas_offset(0);
+            make.left.mas_offset(8);
+            make.bottom.mas_offset(0);
+            make.right.mas_offset(-8);
+        }];
     }
     return self;
 }
@@ -45,46 +47,60 @@
 }
 
 - (void)createItemView:(UIView *)itemView forMarqueeView:(RKMarqueeView *)marqueeView {
-    UILabel *content = [[UILabel alloc] initWithFrame:itemView.bounds];
-    content.font = [UIFont systemFontOfSize:18.0f];
-    content.tag = 2345;
+    UILabel *content = [[UILabel alloc] init];
+    content.font = [UIFont systemFontOfSize:20];
+    content.text = [self.chapter.title stringByTrimmingWhitespaceAndAllNewLine];
     if ([[RKUserConfig sharedInstance].bgImageName isEqualToString:@"reader_bg_2"] || [[RKUserConfig sharedInstance].bgImageName isEqualToString:@"black"]) {
-        content.textColor = [UIColor colorWithHexString:@"ffffff" withAlpha:0.6f];
+        content.textColor = [UIColor colorWithHexString:@"ffffff"];
+    } else {
+        content.textColor = [UIColor colorWithHexString:@"000000"];
     }
-    
     [itemView addSubview:content];
+    [content mas_makeConstraints:^(MASConstraintMaker *make) {
+        make.edges.equalTo(itemView);
+    }];
 }
 
 - (void)updateItemView:(UIView *)itemView atIndex:(NSUInteger)index forMarqueeView:(RKMarqueeView *)marqueeView {
-    UILabel *content = [itemView viewWithTag:2345];
-    content.text = [self.chapter.title stringByTrimmingCharactersInSet];
+    for (UIView *sub in itemView.subviews) {
+        if ([sub isKindOfClass:[UILabel class]]) {
+            UILabel *label = (UILabel *)sub;
+            label.text = [self.chapter.title stringByTrimmingWhitespaceAndAllNewLine];
+        }
+    }
 }
 
 - (CGFloat)itemViewWidthAtIndex:(NSUInteger)index forMarqueeView:(RKMarqueeView *)marqueeView {
     // 指定条目在显示数据源内容时的视图宽度，仅[UUMarqueeViewDirectionLeftward]时被调用。
-    // ### 在数据源不变的情况下，宽度可以仅计算一次并缓存复用。
-    UILabel *content = [[UILabel alloc] init];
-    content.font = [UIFont systemFontOfSize:18.0f];
-    content.text = self.chapter.title;
-    return content.intrinsicContentSize.width;
+    // 在数据源不变的情况下，宽度可以仅计算一次并缓存复用。
+    return self.itemViewWidth;
 }
 
 #pragma mark - setting
 - (void)setChapter:(RKChapter *)chapter {
     _chapter = chapter;
-    self.titleView.text = [self.chapter.title stringByTrimmingCharactersInSet];
-    [self.titleMarqueeView reloadData];
+    
+    self.titleView.text = [self.chapter.title stringByTrimmingWhitespaceAndAllNewLine];
+    self.itemViewWidth = [self.titleView.text boundingRectWithSize:CGSizeMake(1000, 50) options:NSStringDrawingUsesLineFragmentOrigin attributes:@{NSFontAttributeName:[UIFont systemFontOfSize:20]} context:nil].size.width;
+    
+    if (self.isCurrent) {
+        [self.titleMarqueeView reloadData];
+    }
 }
 
 - (void)setIsCurrent:(BOOL)isCurrent {
     _isCurrent = isCurrent;
     
-    if (_isCurrent) {
-        if ([RKUserConfig sharedInstance].isChapterListAutoScroll) {
-            [self.titleMarqueeView start];
-        }
+    if (isCurrent) {
+        self.accessoryType = UITableViewCellAccessoryCheckmark;
+        self.titleView.hidden = YES;
+        self.titleMarqueeView.hidden = NO;
+        [self.titleMarqueeView reloadData];
+        [self.titleMarqueeView start];
     } else {
-        [self.titleMarqueeView pause];
+        self.accessoryType = UITableViewCellAccessoryNone;
+        self.titleView.hidden = NO;
+        self.titleMarqueeView.hidden = YES;
     }
 }
 
@@ -96,8 +112,9 @@
         _titleMarqueeView.timeIntervalPerScroll = 0.0f;
         _titleMarqueeView.userInteractionEnabled = NO;
         _titleMarqueeView.scrollSpeed = 60.0f;
-        _titleMarqueeView.itemSpacing = 20.0f;
+        _titleMarqueeView.itemSpacing = 10.0f;
         _titleMarqueeView.delegate = self;
+        _titleMarqueeView.hidden = YES;
     }
     return _titleMarqueeView;
 }
@@ -108,6 +125,8 @@
         _titleView.font = [UIFont systemFontOfSize:18.0f];
         if ([[RKUserConfig sharedInstance].bgImageName isEqualToString:@"reader_bg_2"] || [[RKUserConfig sharedInstance].bgImageName isEqualToString:@"black"]) {
             _titleView.textColor = [UIColor colorWithHexString:@"ffffff" withAlpha:0.6f];
+        } else {
+            _titleView.textColor = [UIColor colorWithHexString:@"000000"];
         }
     }
     return _titleView;
